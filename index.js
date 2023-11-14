0c423a27e9c9f1ba1aac51a542674b5ee8be5c01
cianwalker <cian0o@icloud.com>
2023-11-14 15:51:51 +0000

    implementing minimal front and back end state

--- index.js
+++ index.js
@@ -5,13 +5,38 @@ const port = 8888;
 
 const clientId = '34c8389b-6033-4f2a-9c96-8a32214a9065';
 const clientSecret = 'c1GzSAwRo6';
-const oidcDiscoveryUrl = 'https://cian.verify.ibm.com';
+const oidcDiscoveryUrl = 'https://cian.verify.ibm.com/oauth2';
 
 let client, oidcIssuer;
 
+Issuer.discover(oidcDiscoveryUrl)
+    .then((issuer) => {
+        oidcIssuer = issuer;
+        client = new issuer.Client({client_id: clientId, client_secret: clientSecret});
+    })
+    .catch((err) => console.error('Error discovering OIDC issuer', err));
 
 app.get('/', (req, res) => {
-    res.send("Welcome to the OIDC Demo")
+    res.sendFile(path.join(__dirname, '/index.html'))
+    // res.send("Welcome to the OIDC Demo")
+
+})
+
+app.get('/login', (req, res) => {
+    const authUrl = client.authorizationUrl({
+        redirect_uri: 'http://localhost:8888/callback',
+        scope: 'openid profile email',
+        // code_challenge,
+        code_challenge_method: 's256',
+        state: 'blankState'
+    });
+    res.redirect(authUrl);
+});
+
+app.get('/callback', async (req, res) =>{
+    const params = client.callbackParams(req);
+    const tokenSet = await client.callback('http://localhost:8888/callback', params, {code_verifier: undefined});
+    res.json(tokenSet);
 })
 
 app.listen(port, () => {
