36c221b567ef2cb3467df0d49fe5146d48266579
cianwalker <cian0o@icloud.com>
2023-11-21 12:05:46 +0000

    Still struggling to pass state argument

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const session = require('express-session');
 const {Issuer, Strategy} = require ('openid-client');
 const app = express();
+const path = require('path');
 const port = 8888;
 
 const clientId = '34c8389b-6033-4f2a-9c96-8a32214a9065';
@@ -8,6 +10,8 @@ const clientSecret = 'c1GzSAwRo6';
 const oidcDiscoveryUrl = 'https://cian.verify.ibm.com/oauth2';
 
 let client, oidcIssuer;
+
+app.use(session({ secret: 'your-secret-key', resave: true, saveUninitialized: true }));
 
 Issuer.discover(oidcDiscoveryUrl)
     .then((issuer) => {
@@ -23,22 +27,41 @@ app.get('/', (req, res) => {
 })
 
 app.get('/login', (req, res) => {
-    const authUrl = client.authorizationUrl({
+    const state = Math.random().toString(36).substring(7);
+    req.session.state = state;
+
+    const authorizationUrl = client.authorizationUrl({
         redirect_uri: 'http://localhost:8888/callback',
         scope: 'openid profile email',
-        // code_challenge,
         code_challenge_method: 's256',
-        state: 'blankState'
+        state: state
     });
-    res.redirect(authUrl);
+
+    console.log('Redirecting to authorization URL:', authorizationUrl);
+
+    res.redirect(authorizationUrl);
 });
 
 app.get('/callback', async (req, res) =>{
-    const params = client.callbackParams(req);
-    const tokenSet = await client.callback('http://localhost:8888/callback', params, {code_verifier: undefined});
-    res.json(tokenSet);
+    try{
+        const state = req.query.state || req.session.state;
+        if(!state){
+            return res.status(400).send('Missing State Parameter');
+        }
+        const params = client.callbackParams(req);
+        const tokenSet = await client.callback('http://localhost:8888/callback', params, {code_verifier: undefined});
+
+        res.json(tokenSet);
+        console.log('Callback params', params);
+        console.log('Callback state', state);
+    } catch (err){
+        console.error('Error in callback:', err);
+        res.status(500).send('Internal server error');
+    }
 })
 
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`)
+
+
 })
